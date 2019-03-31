2165ac0120dc509128084d8fd1ce51d98e92001f
Wade Li <wadeli@wades-air>
2019-03-31 19:10:07 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Wade Li on 3/31/19.
+//  Copyright © 2019 Wade Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
